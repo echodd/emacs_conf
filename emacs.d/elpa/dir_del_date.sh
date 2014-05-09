d9f45b158c81d5257c585fe7d1c3e6d659b34739
echodd <zxd641326@163.com>
2014-05-09 11:46:11 +0800

    elpa中保留原始包名，方便elpa升级，但是使用时用plugins中去掉后缀的目录

--- emacs.d/elpa/dir_del_date.sh
+++ emacs.d/elpa/dir_del_date.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+plugins=`pwd`"../plugins/"
 for tmp in `ls`
 do
 	# find the dir 
 	if [ -d $tmp ];then
-		echo $tmp | perl -lne 'if(/[0-9]/){$old=$_;$_=~s/-[0-9].*$//g;`mv "$old" "$_"`}'
+		echo $tmp | perl -lne 'if(/[0-9]/){$old=$_;$_=~s/-[0-9].*$//g;`cp -r  "$old" "../plugins/$_"`}'
+#		echo $tmp | perl -lne 'if(/[0-9]/){$old=$_;$_=~s/-[0-9].*$//g;`mv  "$old" "$_"`}'
  	fi
 done
